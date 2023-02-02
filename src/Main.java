df8ae11a50ca88b47121438b38479bc7a43cb8bf
oksana <oksana3323@gmail.com>
2023-02-02 14:36:07 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+     Person person=new Person();
+       person.firstname="Aizat";
+        person.lastname="Duisheeva";
+  person.age=19;
+  person.gender="jen";
+   person.country="Kyrgyzstan";
+     person.birth="Mart";
+        System.out.println(person.work(" Peaksoft "));
+        System.out.println(person.eat(" pizza "));
+
+
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,20 @@
+public class Person {
+    String firstname;
+    String lastname;
+    int age;
+    String gender;
+    String country;
+    String birth;
+
+    String work(String work) {
+
+
+        return firstname +"  "+ lastname + work+"ishteit";
+    }
+
+    String eat(String food) {
+       return firstname +"  "+lastname +food+"jakshy korot";
+
+    }}
+
+
